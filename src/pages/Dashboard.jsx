8727107ecd99cad9a73e9d339e35a21acbb597d7
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-30 14:13:11 +0300

    aa

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext';
+import loading from "../assets/loading.gif";
+import { BlogContext } from '../contexts/BlogContext';
+import BlogCard from '../components/BlogCard';
+
+const Dashboard = () => {
+  const user = useContext(AuthContext);
+  console.log("Dashboard User: ", user);
+
+  const {GetBlogs} = useContext(BlogContext);
+  const {blogList, isLoading} = GetBlogs();
+
+  return (
+    <div>
+      <h3 style={{textAlign: "center", 
+                  backgroundColor: "yellow"}}>
+          ──── Dashboard ────
+      </h3>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "2rem",
+          // margin: "3rem",
+          justifyContent: "center",
+        }}
+      >
+        {isLoading ? (
+          <img src={loading} alt="loadingSpinner" />
+        ) : (
+          <>
+            {blogList?.map((item, index) => (
+              <BlogCard item={item} key={index} />
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard
